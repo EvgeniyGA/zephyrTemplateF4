5970eed9e2335679ec6f49f9d5f54f20fb8c8300
EvgeniyGA <93276404+EvgeniyGA@users.noreply.github.com>
2025-11-29 15:14:06 +0300

    add-lfs

--- src/main.c
+++ src/main.c
@@ -19,6 +19,10 @@
 #include <zephyr/data/json.h>
 #include <zephyr/sys/util_macro.h>
 #include <zephyr/net/net_config.h>
+#include <zephyr/fs/fs.h>
+#include <zephyr/fs/littlefs.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/storage/flash_map.h>
 
 #if CONFIG_USB_DEVICE_STACK_NEXT
 #include <sample_usbd.h>
@@ -295,9 +299,93 @@ static int init_usb(void)
 	return 0;
 }
 
+
+#ifdef CONFIG_APP_LITTLEFS_STORAGE_FLASH
+static int littlefs_flash_erase(unsigned int id)
+{
+	const struct flash_area *pfa;
+	int rc;
+
+	rc = flash_area_open(id, &pfa);
+	if (rc < 0) {
+		LOG_ERR("FAIL: unable to find flash area %u: %d\n",
+			id, rc);
+		return rc;
+	}
+
+	LOG_PRINTK("Area %u at 0x%x on %s for %u bytes\n",
+		   id, (unsigned int)pfa->fa_off, pfa->fa_dev->name,
+		   (unsigned int)pfa->fa_size);
+
+	/* Optional wipe flash contents */
+	if (IS_ENABLED(CONFIG_APP_WIPE_STORAGE)) {
+		rc = flash_area_flatten(pfa, 0, pfa->fa_size);
+		LOG_ERR("Erasing flash area ... %d", rc);
+	}
+
+	flash_area_close(pfa);
+	return rc;
+}
+#define PARTITION_NODE DT_NODELABEL(lfs1)
+
+#if DT_NODE_EXISTS(PARTITION_NODE)
+FS_FSTAB_DECLARE_ENTRY(PARTITION_NODE);
+#else /* PARTITION_NODE */
+FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
+static struct fs_mount_t lfs_storage_mnt = {
+	.type = FS_LITTLEFS,
+	.fs_data = &storage,
+	.storage_dev = (void *)FIXED_PARTITION_ID(storage_partition),
+	.mnt_point = "/lfs",
+};
+#endif /* PARTITION_NODE */
+
+	struct fs_mount_t *mountpoint =
+#if DT_NODE_EXISTS(PARTITION_NODE)
+		&FS_FSTAB_ENTRY(PARTITION_NODE)
+#else
+		&lfs_storage_mnt
+#endif
+		;
+
+static int littlefs_mount(struct fs_mount_t *mp)
+{
+	int rc;
+
+	rc = littlefs_flash_erase((uintptr_t)mp->storage_dev);
+	if (rc < 0) {
+		return rc;
+	}
+
+	/* Do not mount if auto-mount has been enabled */
+#if !DT_NODE_EXISTS(PARTITION_NODE) ||						\
+	!(FSTAB_ENTRY_DT_MOUNT_FLAGS(PARTITION_NODE) & FS_MOUNT_FLAG_AUTOMOUNT)
+	rc = fs_mount(mp);
+	if (rc < 0) {
+		LOG_PRINTK("FAIL: mount id %" PRIuPTR " at %s: %d\n",
+		       (uintptr_t)mp->storage_dev, mp->mnt_point, rc);
+		return rc;
+	}
+	LOG_PRINTK("%s mount: %d\n", mp->mnt_point, rc);
+#else
+	LOG_PRINTK("%s automounted\n", mp->mnt_point);
+#endif
+
+	return 0;
+}
+#endif /* CONFIG_APP_LITTLEFS_STORAGE_FLASH */
+
+
 int main(void)
 {
+	int rc;
+
+	LOG_PRINTK("Sample program to r/w files on littlefs\n");
 //	init_usb();
+	rc = littlefs_mount(mountpoint);
+	if (rc < 0) {
+		return 0;
+	}
 
 	http_server_start();
 	return 0;
